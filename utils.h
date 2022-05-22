bb70a8c50103dbe3f1f8ac6b084582aea6c2b04d
MohammedBouzaid <bouzidmohamed7@gmail.com>
2022-05-22 17:57:35 +0100

    Removed unnecessary functions definitions from the `utils.h` header file.

--- utils.h
+++ utils.h
@@ -2,21 +2,7 @@
 
 using namespace std;
 
-int encrypt(unsigned char *plaintext, 
-            int plaintext_len, 
-            unsigned char *key,
-            unsigned char *iv, 
-            unsigned char *ciphertext);
-
-int decrypt(unsigned char *ciphertext,
-            int ciphertext_len, 
-            unsigned char *key,
-            unsigned char *iv, 
-            unsigned char *plaintext);
-
 void handleErrors();
-
-void hashPassword(const char *string, char outputBuffer[65]);
 
 string myEncrypt(string plainText, string pass);
 
